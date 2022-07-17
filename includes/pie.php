eea572f28ff7b86e243e5e4421650363ac1293c4
Lucas Rodriguez <rodriguezlucas@outlook.com.ar>
2022-07-16 21:36:44 -0300

    primer commit

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Lucas Rodriguez &copy; 2018</p>
+		</footer>
+		
+	</body>
+</html>
